80920221e9f690ef84f7ad5a4840e6c00fe95099
msmadds <103424445+msmadds@users.noreply.github.com>
2024-06-25 12:04:12 +0300

    Create get_license_info_junos.py
    Using the RPC get_license_summary_information

---
+++ Junos-scripts/get_license_info_junos.py
@@ -0,0 +1,32 @@
+from jnpr.junos import Device
+from lxml import etree
+from pprint import pprint
+import getpass
+
+
+def get_license_info(host, username, password,port):
+    with Device(host=host, user=username, password=password, port=port, gather_facts=False) as dev:
+        # rpc call to return object in text fformat, and the data type for op will be  bytes
+        output= dev.rpc.get_license_summary_information({'format':'text'})
+        # To print object returned as bytes, uncomment below line (etree.tostring)
+        #pprint (etree.tostring(output))
+        #to assign the output to a variable so we can manipulate the output
+        license_ = etree.tostring(output)
+        ot = str(license_)
+        output_=ot.split('\\n')
+        for line in output_:
+            print(line)
+    
+def main():
+    password  = getpass.getpass()
+    username = input("insert username: ")
+    host= 'x.x.x.x'
+    port = '22'  #incase the device is not using the default netconf port
+    get_license_info(host,username,password,port)
+
+if __name__ == '__main__':
+    main()
+
+
+
+
